2c6d6aa44dba8a17a2e5573c6c9944ba00757edc
rurito0125 <rurito.blog@gmail.com>
2020-09-21 02:32:17 +0900

    updated

--- src/components/window/member_window.js
+++ src/components/window/member_window.js
@@ -4,9 +4,14 @@ import styles from './member_window.module.scss'
 const MemberWindow = () => {
 
   return (
-    <React.Fragment className={styles.root}>
-      <h1>メンバー紹介</h1>
-    </React.Fragment>
+    <>
+      <div className={styles.root}>
+        <div className={styles.main} >
+          <h2 className={styles.title}>メンバー紹介 <br/>(非公式)ファンサイト</h2>
+          <p className={styles.content}>さんばかの皆さんがデビューしてから<br />1.5周年をお祝いする非公式ファンサイト</p>
+        </div>
+      </div>
+    </>
   );
 };
 
