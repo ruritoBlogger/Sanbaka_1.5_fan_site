70a7e35fba5e7a371fe798539088a7c686e35815
rurito0125 <rurito.blog@gmail.com>
2020-09-20 14:03:24 +0900

    メンバー紹介ページを作成

---
+++ src/pages/member.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Member = () => (
+  <div>
+    <p>Member page</p>
+  </div>
+);
+
+export default Member;
+
--- src/root.js
+++ src/root.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/header';
 import Home from './pages/home';
+import Member from './pages/member';
 
 const Notfound = () => (
   <div> 404 not found</div>
@@ -13,6 +14,7 @@ const Root = () => (
     <Header />
     <Switch>
       <Route exact path="/" render={() => <Home />} />
+      <Route exact path="/member" render={() => <Member />} />
       <Route component={Notfound} />
     </Switch>
   </Router>
