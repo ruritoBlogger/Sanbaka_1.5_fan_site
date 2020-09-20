99b7eac26744d70cc378ce3abf83746a8b92fcdb
rurito0125 <rurito.blog@gmail.com>
2020-09-20 15:57:10 +0900

    button:hoverのcssを設定

--- src/components/parts/button.js
+++ src/components/parts/button.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -12,6 +11,7 @@ const useStyles = makeStyles((theme) => ({
     "border-radius": 0,
     "font-size": "25px",
     "background": "transparent",
+    "padding-bottom": "10px",
     "&:hover": {
       "border-bottom": "solid 3px",
     },
