9861955350e3931655c30f4583df204498d2f7f8
rurito0125 <rurito.blog@gmail.com>
2020-09-28 02:21:18 +0900

    集合写真を表示する部分を実装

--- src/components/window/member/member_window.js
+++ src/components/window/member/member_window.js
@@ -9,16 +9,26 @@ const MemberWindow = (props) => {
           {(() => {
             if( props.isRight ) {
               return (
-                <div className={styles.main} >
-                  <h2 className={`${styles.title} ${styles.right}`}>メンバー紹介</h2>
-                  <p className={`${styles.content} ${styles.right}`}>いい感じの紹介文が欲しいわね...</p>
+                <div className={`${styles.main} ${styles.flex}`} >
+                  <div className={styles.right_padding}>
+                    <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
+                  </div>
+                  <div>
+                    <h2 className={`${styles.title} ${styles.right}`}>メンバー紹介</h2>
+                    <p className={`${styles.content} ${styles.right}`}>いい感じの紹介文が欲しいわね...</p>
+                  </div>
                 </div>
               )
             } else {
               return (
-                <div className={styles.main} >
-                  <h2 className={styles.title}>メンバー紹介</h2>
-                  <p className={styles.content}>いい感じの紹介文が欲しいわね...</p>
+                <div className={`${styles.main} ${styles.flex}`} >
+                  <div className={styles.right_padding}>
+                    <h2 className={styles.title}>メンバー紹介</h2>
+                    <p className={styles.content}>いい感じの紹介文が欲しいわね...</p>
+                  </div>
+                  <div>
+                    <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
+                  </div>
                 </div>
               )
             }
