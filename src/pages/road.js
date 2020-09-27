5f9d4640438b6249c82b7f8352195ad4abeff9df
rurito0125 <rurito.blog@gmail.com>
2020-09-27 22:42:13 +0900

    1.5周年までのページにてwindowを組み込み

--- src/pages/road.js
+++ src/pages/road.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+import HistoryWindow from '../components/window/history/history_window';
+import Header from '../components/header'
 
-const Road = () => (
-  <div>
-    <p>Road page</p>
-  </div>
-);
+const Road = () => {
+
+  return (
+    <>
+      <Header />
+      <Scrollbars autoHeight autoHeightMin={975}>
+        <HistoryWindow />
+      </Scrollbars>
+    </>
+  )
+};
 
 export default Road;
