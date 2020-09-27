330feb829abf65adbb7fae30d2d2f7be3c2dcaaa
rurito0125 <rurito.blog@gmail.com>
2020-09-27 22:47:10 +0900

    アーカイブのコンポーネントを作成して組み込み

---
+++ src/components/parts/archive.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './archive.module.scss';
+
+const Archive = (props) => {
+
+  return (
+    <>
+      <p>アーカイブとか</p>
+    </>
+  )
+};
+
+export default Archive;
--- src/pages/road.js
+++ src/pages/road.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 import HistoryWindow from '../components/window/history/history_window';
+import Archive from '../components/parts/archive';
 import Header from '../components/header'
 
 const Road = () => {
@@ -10,6 +11,7 @@ const Road = () => {
       <Header />
       <Scrollbars autoHeight autoHeightMin={975}>
         <HistoryWindow />
+        <Archive />
       </Scrollbars>
     </>
   )
