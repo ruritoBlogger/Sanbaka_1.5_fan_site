7179082b308e3744737d202563015feb3c85c9f0
rurito0125 <rurito.blog@gmail.com>
2020-09-28 03:21:43 +0900

    背景色を追加

--- src/components/window/member/member_window.js
+++ src/components/window/member/member_window.js
@@ -10,10 +10,10 @@ const MemberWindow = (props) => {
             if( props.isRight ) {
               return (
                 <div className={`${styles.main} ${styles.flex}`} >
-                  <div className={`${styles.right_padding} ${styles.frame}`}>
+                  <div className={styles.frame}>
                     <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
                   </div>
-                  <div>
+                  <div className={styles.left_padding}>
                     <h2 className={`${styles.title} ${styles.right}`}>メンバー紹介</h2>
                     <p className={`${styles.content} ${styles.right}`}>いい感じの紹介文が欲しいわね...</p>
                   </div>
