69b71aee37a278dbfab08fc4e4b3ca4724846371
rurito <rurito.blog@gmail.com>
2020-09-28 22:34:57 +0900

    updated

--- src/components/window/history/history_window.js
+++ src/components/window/history/history_window.js
@@ -6,9 +6,33 @@ const HistoryWindow = () => {
   return (
     <>
       <div className={styles.root}>
-        <div className={styles.main} >
-          <h2 className={styles.title}>1.5周年までの道のり</h2>
-          <p className={styles.content}>いい感じの紹介文が欲しいわね...</p>
+        <div className={`${styles.main} ${styles.flex}`} >
+          <div className={styles.right_padding}>
+            <h2 className={styles.title}>1.5周年までの道のり</h2>
+            <p className={styles.content}>いい感じの紹介文が欲しいわね...</p>
+          </div>
+          <div className={styles.flex_column}>
+            <div className={styles.flex_img}>
+              <div className={`${styles.grid_one}`}>
+                <img className={styles.img_small} src="https://i.ytimg.com/vi/ZlpG8R_biw4/hqdefault.jpg" />
+              </div>
+            </div>
+            <div className={styles.flex_img}>
+              <div className={`${styles.grid_two}`}>
+                <img className={styles.img_small} src="https://i.ytimg.com/vi/ZlpG8R_biw4/hqdefault.jpg" />
+              </div>
+            </div>
+            <div className={styles.flex_img}>
+              <div className={`${styles.grid_three}`}>
+                <img className={styles.img_small} src="https://i.ytimg.com/vi/ZlpG8R_biw4/hqdefault.jpg" />
+              </div>
+            </div>
+            <div className={styles.flex_img}>
+              <div className={`${styles.grid_four}`}>
+                <img className={styles.img_small} src="https://i.ytimg.com/vi/ZlpG8R_biw4/hqdefault.jpg" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
