c5e7d825c5d39741bc2df2c5e9620c7837387aa9
rurito0125 <rurito.blog@gmail.com>
2020-09-29 00:42:08 +0900

    アンジュの説明部分を実装

--- src/components/window/member/ange_window.js
+++ src/components/window/member/ange_window.js
@@ -9,7 +9,10 @@ const AngeWindow = () => {
         <div className={styles.main} >
           <h3 className={`${styles.subtitle} ${styles.right}`}>公式美少女錬金術師ライバー</h3>
           <h2 className={`${styles.title} ${styles.right}`}>アンジュ・カトリーナ</h2>
-          <p className={`${styles.content} ${styles.right}`}>いい感じの紹介文が欲しいわね...</p>
+          <p className={`${styles.content} ${styles.right}`}>
+            ボロボロの小屋で時間を忘れて<br/>錬金術の研究に明け暮れている.<br/>
+            大人っぽい女性的な体に憧れており<br/>実はその研究をしているとかしていないとか.
+          </p>
         </div>
       </div>
     </>
