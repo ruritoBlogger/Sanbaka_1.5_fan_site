e616e2899b08b7108165b5c7ee3a2fab1d85ac3f
rurito0125 <rurito.blog@gmail.com>
2020-09-29 02:45:20 +0900

    アニメーションを実装

--- src/pages/member.js
+++ src/pages/member.js
@@ -1,21 +1,84 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MemberWindow from '../components/window/member/member_window';
 import AngeWindow from '../components/window/member/ange_window';
 import LizeWindow from '../components/window/member/lize_window';
 import InuiWindow from '../components/window/member/inui_window';
 import Header from '../components/header'
 import Footer from '../components/footer';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
-const Member = () => (
-  <>
-    <Header />
-    <MemberWindow />
-    <AngeWindow />
-    <LizeWindow />
-    <InuiWindow />
-    <Footer />
-  </>
-);
+const Member = () => {
+  const headerRef = useRef(null);
+  const revealRefs = useRef([]);
+
+  const windows = [
+    {
+      key: "member",
+      content: <MemberWindow />
+    },
+    {
+      key: "ange",
+      content: <AngeWindow />
+    },
+    {
+      key: "lize",
+      content: <LizeWindow />
+    },
+    {
+      key: "inui",
+      content: <InuiWindow />
+    }
+  ]
+
+  useEffect(() => {
+
+    gsap.from(headerRef.current, {
+      autoAlpha: 0,
+      ease: 'none',
+      delay: 1
+    });
+
+    revealRefs.current.forEach((el, index) => {
+
+      gsap.fromTo(el, {
+        autoAlpha: 0
+      }, {
+        duration: 1,
+        autoAlpha: 1,
+        ease: 'none',
+        scrollTrigger: {
+          id: `section-${index+1}`,
+          trigger: el,
+          start: 'top center+=100',
+          toggleActions: 'play none none reverse'
+        }
+      });
+
+    });
+
+  }, []);
+
+  const addToRefs = el => {
+    if (el && !revealRefs.current.includes(el)) {
+        revealRefs.current.push(el);
+    }
+  };
+  return (
+    <>
+      <Header />
+      {
+        windows.map(({key, content}) => (
+          <div key={key} ref={addToRefs}>
+            {content}
+          </div>
+        ))
+      }
+      <Footer />
+    </>
+  );
+};
 
 export default Member;
 
