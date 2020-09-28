1835d27a3c01f55fb3c1c2b41e6ee605dc74de72
rurito <rurito.blog@gmail.com>
2020-09-28 16:07:31 +0900

    headerのmoduleを組み込んだ

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import HeaderButton from './parts/header_button';
+import Headroom from 'react-headroom';
 import styles from './header.module.scss';
 
 const Header = () => {
@@ -24,12 +25,12 @@ const Header = () => {
 
   return (
     <>
-      <nav className={styles.root}>
+      <Headroom className={styles.root}>
         <HeaderButton handleClick={ () => MoveHomePage()} msg="さんばか" />
         <HeaderButton handleClick={ () => MoveMemberPage()} msg="愉快な仲間たち" />
         <HeaderButton handleClick={ () => MoveRoadPage()} msg="1.5周年までの道のり" />
         <HeaderButton handleClick={() => MoveSitePage()} msg="このサイトについて" />
-      </nav>
+      </Headroom>
     </>
   )
 };
