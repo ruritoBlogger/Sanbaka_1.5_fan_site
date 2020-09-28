11b04fe22bbdabb63a15aaf54c35e695c1378f83
rurito0125 <rurito.blog@gmail.com>
2020-09-29 08:07:58 +0900

    updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {isMobile} from 'react-device-detect';
+import Smartphone from './smartphone';
 import Root from './root';
 import './index.scss';
 
-const App = () => (
-  <>
-    {(() => {
-      if( navigator.userAgent.match(/(iPhone|iPod|Android.*Mobile)/i)){
-        return <Smartphone />
-      }else{
-        return <Root />
-      }
-    })()}
-  </>
-);
+const App = () => {
+  return (
+    <>
+      {(() => {
+        if (isMobile) {
+          return (
+            <div>
+              このサイトはpcのみに対応しています.
+              大変申し訳無いのですが, pcからアクセスしていただけると助かります.
+            </div>
+          );
+        }else{
+          return <Root />
+        }
+      })()}
+    </>
+  );
+};
 
 const app = document.getElementById('app'); // eslint-disable-line no-undef
 ReactDOM.render(
