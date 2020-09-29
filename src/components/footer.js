5511e7f6558ccbfdd137840a1d76c90741f6bbdf
rurito0125 <rurito.blog@gmail.com>
2020-09-29 23:20:21 +0900

    final commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,9 @@ const Footer = () => {
   return (
     <>
       <div className={styles.root}>
+        <hr />
         <div className={styles.content} >
-          <p className={styles.text}>本サイトは, さんばかの1ファンが個人で作成したものになります. </p>
+          <p className={styles.text}><br/>本サイトは, さんばかの1ファンが個人で作成したものになります. </p>
           <p className={styles.text}>また, 本サイトはいちから株式会社及びにじさんじプロジェクトとは一切関わりはありません.</p>
           <p className={styles.text}>本サイトの不具合報告などは<a href="https://twitter.com/rurito8">こちら</a>のTwitterアカウントまでお願いします.</p>
         </div>
--- src/pages/site.js
+++ src/pages/site.js
@@ -1,34 +1,110 @@
-import React from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
+import React, { useEffect, useRef } from 'react';
 import styles from './site.module.scss';
 import Header from '../components/header'
 import Footer from '../components/footer';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
 
 const Site = () => {
+  const headerRef = useRef(null);
+  const revealRefs = useRef([]);
+
+  const windows = [
+    {
+      key: "key",
+      content: <div className={styles.root}>
+                <div className={styles.block}>
+                  <h3 className={styles.subtitle}>使用させていただいたサイト</h3>
+                  <p className={styles.content}>
+                    配信情報など:
+                    <a className={styles.link} href="https://wikiwiki.jp/nijisanji/">にじさんじ非公式wiki</a>
+                  </p>
+                  <p className={styles.content}>
+                    立ち絵:
+                    <a className={styles.link} href="https://nijisanji.ichikara.co.jp/member/">にじさんじ公式サイト</a>
+                  </p>
+                  <p className={styles.content}>
+                    イメージカラー:
+                    <a className={styles.link} href="https://www.itsukaralink.jp/livers">いつから.link</a>
+                  </p>
+                </div>
+              </div>
+    },
+    {
+      key: "thank",
+      content:<div className={styles.root}>
+                <div className={styles.block}>
+                  <h3 className={styles.subtitle} >サイト制作者より</h3>
+                  <p className={styles.content}>
+                    本サイトにアクセスしていただきありがとうございます.<br/>
+                    本サイトから少しでもさんばかの魅力が伝わると嬉しいです.
+                  </p>
+                </div>
+              </div>
+    },
+    {
+      key: "test",
+      content:<div>
+                <div className={styles.root}>
+                  <div className={styles.block}>
+                    <h3 className={styles.subtitle} >最後に</h3>
+                    <p className={styles.content}>
+                      今回1.5周年を迎えましたさんばかの３人の<br/>
+                      今後のご活躍を応援しています.<br/><br/>
+                    </p>
+                  </div>
+                </div>
+                <Footer />
+              </div>
+    }
+  ]
+
+  useEffect(() => {
+
+    gsap.from(headerRef.current, {
+      autoAlpha: 0,
+      ease: 'none',
+      delay: 1
+    });
+
+    revealRefs.current.forEach((el, index) => {
+
+      gsap.fromTo(el, {
+        autoAlpha: 0
+      }, {
+        duration: 1,
+        autoAlpha: 1,
+        ease: 'none',
+        scrollTrigger: {
+          id: `section-${index+1}`,
+          trigger: el,
+          start: 'top center+=200',
+          toggleActions: 'play none none reverse'
+        }
+      });
+
+    });
+
+  }, []);
+
+  const addToRefs = el => {
+    if (el && !revealRefs.current.includes(el)) {
+        revealRefs.current.push(el);
+    }
+  };
 
   return (
     <>
       <Header />
-      <Scrollbars autoHeight autoHeightMin={975}>
-        <div className={styles.root}>
-          <div className={styles.block}>
-            <h3 className={styles.subtitle}>使用させていただいたサイト</h3>
-            <p className={styles.content}>
-              配信情報など:
-              <a className={styles.link} href="https://wikiwiki.jp/nijisanji/">にじさんじ非公式wiki</a>
-            </p>
-            <p className={styles.content}>
-              立ち絵:
-              <a className={styles.link} href="https://nijisanji.ichikara.co.jp/member/">にじさんじ公式サイト</a>
-            </p>
-            <p className={styles.content}>
-              イメージカラー:
-              <a className={styles.link} href="https://www.itsukaralink.jp/livers">いつから.link</a>
-            </p>
+      {
+        windows.map(({key, content}) => (
+          <div key={key} ref={addToRefs}>
+            {content}
           </div>
-        </div>
-        <Footer />
-      </Scrollbars>
+        ))
+      }
     </>
   )
 };
