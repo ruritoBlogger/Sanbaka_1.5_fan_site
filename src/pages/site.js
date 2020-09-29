310ae4bb010f83f66900ea0c993b8b60c2c6ed64
rurito0125 <rurito.blog@gmail.com>
2020-09-29 09:01:24 +0900

    使用させていただいたサイトについて書いた

--- src/pages/site.js
+++ src/pages/site.js
@@ -12,16 +12,19 @@ const Site = () => {
       <Scrollbars autoHeight autoHeightMin={975}>
         <div className={styles.root}>
           <div className={styles.block}>
-            <h2 className={styles.title}>このサイトについて</h2>
-            <p className={styles.content}>このサイトは〜</p>
-            <p className={styles.content}>このサイトは〜</p>
-            <p className={styles.content}>このサイトは〜</p>
-          </div>
-          <div className={styles.block}>
-            <h3 className={styles.subtitle}>使用させていただいた素材</h3>
-            <p className={styles.content}>このサイトは〜</p>
-            <p className={styles.content}>このサイトは〜</p>
-            <p className={styles.content}>このサイトは〜</p>
+            <h3 className={styles.subtitle}>使用させていただいたサイト</h3>
+            <p className={styles.content}>
+              配信情報など:
+              <a className={styles.link} href="https://wikiwiki.jp/nijisanji/">にじさんじ非公式wiki</a>
+            </p>
+            <p className={styles.content}>
+              立ち絵:
+              <a className={styles.link} href="https://nijisanji.ichikara.co.jp/member/">にじさんじ公式サイト</a>
+            </p>
+            <p className={styles.content}>
+              イメージカラー:
+              <a className={styles.link} href="https://www.itsukaralink.jp/livers">いつから.link</a>
+            </p>
           </div>
         </div>
         <Footer />
