e011e4038f7b2c0a75795b37f99cb4c54649e912
rurito0125 <rurito.blog@gmail.com>
2020-10-15 02:02:38 +0900

    top windowをスマホ版に対応させた

--- src/components/window/top/top_window.js
+++ src/components/window/top/top_window.js
@@ -11,8 +11,8 @@ const TopWindow = () => {
           <p className={`${styles.content} ${styles.center}`}>本サイトはさんばかが結成されてから<br />1.5周年をお祝いする非公式ファンサイトです.</p>
         </div>
       </div>
-      <div className={styles.center}>
-        <a className={styles.scroll} href="#">Scroll</a>
+      <div className={`${styles.center} ${styles.only_pc}`}>
+        <a className={`${styles.scroll} ${styles.only_pc}`} href="#">Scroll</a>
       </div>
     </>
   );
