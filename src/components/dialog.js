c6fa82b5ab534a80247437e80c98461a23873902
rurito0125 <rurito.blog@gmail.com>
2020-10-17 15:33:33 +0900

    スマホ版にてheaderのボタンをクリックした時に表示するダイアログの雛形を作成

---
+++ src/components/dialog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './dialog.module.scss';
+
+const Dialog = () => {
+
+  return (
+    <>
+      <div className={styles.root}>
+        <p>dialog</p>
+      </div>
+    </>
+  )
+}
