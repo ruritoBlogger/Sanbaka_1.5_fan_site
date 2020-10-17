d2876ec5c3650e6cd53b7a5617bc7700e137f53e
rurito0125 <rurito.blog@gmail.com>
2020-10-17 16:44:53 +0900

    ダイアログを開いた時各ページへのリンクを表示するようにした

--- src/components/dialog.js
+++ src/components/dialog.js
@@ -1,9 +1,36 @@
 import React, {useRef, useCallback} from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './dialog.module.scss';
 import Menu from '@material-ui/icons/Menu';
+import HeaderButton from './parts/header_button';
 
 const Dialog = () => {
   const ref = useRef(null);
+  const history = useHistory();
+
+  const MoveTop = () => {
+    window.scrollTo({top: 0, behavior: "smooth"});
+  }
+
+  const MoveMemberPage = () => {
+    window.scrollTo({top: 0});
+    history.push("/member");
+  }
+
+  const MoveRoadPage = () => {
+    window.scrollTo({top: 0});
+    history.push("/road");
+  }
+
+  const MoveSitePage = () => {
+    window.scrollTo({top: 0});
+    history.push("/site");
+  }
+
+  const MoveHomePage = () => {
+    window.scrollTo({top: 0});
+    history.push("/");
+  }
 
   const showModal = useCallback(() => {
     if( ref.current ) {
@@ -25,10 +52,13 @@ const Dialog = () => {
         </button>
       </div>
       <dialog ref={ref} className={styles.root}>
-        <p>dialog</p>
-        <button onClick={closeModal} >
-          close
+        <button className={styles.icon_button} onClick={closeModal} >
+          <Menu />
         </button>
+        <HeaderButton handleClick={ () => MoveHomePage()} msg="トップページ" />
+        <HeaderButton handleClick={ () => MoveMemberPage()} msg="さんばかとは" />
+        <HeaderButton handleClick={ () => MoveRoadPage()} msg="1.5周年までの道のり" />
+        <HeaderButton handleClick={() => MoveSitePage()} msg="このサイトについて" />
       </dialog>
     </>
   )
