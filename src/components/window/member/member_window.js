70d14f5a310a1bd5e4d5a685d3bc16c3d7a1f7ea
rurito0125 <rurito.blog@gmail.com>
2020-10-23 17:31:18 +0900

    一部のscssを修正

--- src/components/window/member/member_window.js
+++ src/components/window/member/member_window.js
@@ -39,7 +39,7 @@ const MemberWindow = (props) => {
           } else {
             return (
               <div className={styles.root} >
-                <div className={`${styles.main} ${styles.flex}`} >
+                <div className={`${styles.main} ${styles.dynamic_flex_reverse}`} >
                   <div className={styles.right_padding}>
                     <h2 className={`${styles.title} ${styles.center}`}>さんばかとは</h2>
                     <p className={`${styles.content} ${styles.center}`}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
@@ -48,13 +48,11 @@ const MemberWindow = (props) => {
                     <p className={`${styles.content} ${styles.center} ${styles.font_inui}`}>戌亥とこ</p>
                     <p className={`${styles.content} ${styles.center}`}><br/>３人のバカという意味ではないとのこと.</p>
                   </div>
-                  <div>
-                    <div className={styles.frame}>
-                      <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
-                    </div>
+                  <div className={styles.frame}>
+                    <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
                   </div>
                 </div>
-                <div className={styles.center}>
+                <div className={styles.center_component}>
                   <a className={styles.scroll} href="#">Scroll</a>
                 </div>
               </div>
