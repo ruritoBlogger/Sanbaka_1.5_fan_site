404eca657c10fce682b11c3faef10908e77006ec
rurito0125 <rurito.blog@gmail.com>
2020-10-23 20:28:46 +0900

    member windowを修正

--- src/components/window/member/member_window.js
+++ src/components/window/member/member_window.js
@@ -16,20 +16,20 @@ const MemberWindow = (props) => {
         {(() => {
           if( props.isRight ) {
             return (
-              <div className={styles.root} >
-                <div className={`${styles.main} ${styles.dynamic_flex}`} >
-                  <div className={styles.frame}>
-                    <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
+              <div className={styles.ly_center} >
+                <div className={styles.bl_memberWindow} >
+                  <div className={styles.bl_memberWindow_frame}>
+                    <img className={styles.bl_memberWindow_img} src={`${window.location.origin}/image/sanbaka.png`} />
                   </div>
                   <div className={styles.left_padding}>
                     <div>
-                      <h2 className={`${styles.title} ${styles.center}`}>さんばかとは</h2>
-                      <p className={`${styles.content} ${styles.center}`}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
-                      <p className={`${styles.content} ${styles.center} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
-                      <p className={`${styles.content} ${styles.center} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
-                      <p className={`${styles.content} ${styles.center} ${styles.font_inui}`}>戌亥とこ</p>
+                      <h2 className={styles.bl_memberWindow_title}>さんばかとは</h2>
+                      <p className={styles.bl_memberWindow_content}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
+                      <p className={`${styles.bl_memberWindow_content} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
+                      <p className={`${styles.bl_memberWindow_content} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
+                      <p className={`${styles.bl_memberWindow_content} ${styles.font_inui}`}>戌亥とこ</p>
                     </div>
-                    <div className={styles.center_component}>
+                    <div className={`${styles.centerComponent} ${styles.top_margin}`}>
                       <Button handleClick={ () => MoveMemberPage()} msg="もっと詳しく" />
                     </div>
                   </div>
@@ -38,23 +38,23 @@ const MemberWindow = (props) => {
             )
           } else {
             return (
-              <div className={styles.root} >
-                <div className={`${styles.main} ${styles.flex}`} >
+              <div className={styles.ly_center} >
+                <div className={styles.bl_memberWindow} >
                   <div className={styles.right_padding}>
-                    <h2 className={`${styles.title} ${styles.center}`}>さんばかとは</h2>
-                    <p className={`${styles.content} ${styles.center}`}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
-                    <p className={`${styles.content} ${styles.center} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
-                    <p className={`${styles.content} ${styles.center} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
-                    <p className={`${styles.content} ${styles.center} ${styles.font_inui}`}>戌亥とこ</p>
-                    <p className={`${styles.content} ${styles.center}`}><br/>３人のバカという意味ではないとのこと.</p>
+                    <h2 className={styles.bl_memberWindow_title}>さんばかとは</h2>
+                    <p className={styles.bl_memberWindow_content}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
+                    <p className={`${styles.bl_memberWindow_content} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
+                    <p className={`${styles.bl_memberWindow_content} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
+                    <p className={`${styles.bl_memberWindow_content} ${styles.font_inui}`}>戌亥とこ</p>
+                    <p className={styles.bl_memberWindow_content}><br/>３人のバカという意味ではないとのこと.</p>
                   </div>
                   <div>
-                    <div className={styles.frame}>
-                      <img className={styles.img} src={`${window.location.origin}/image/sanbaka.png`} />
+                    <div className={styles.bl_memberWindow_frame}>
+                      <img className={styles.bl_memberWindow_img} src={`${window.location.origin}/image/sanbaka.png`} />
                     </div>
                   </div>
                 </div>
-                <div className={styles.center}>
+                <div className={styles.ly_center}>
                   <a className={styles.scroll} href="#">Scroll</a>
                 </div>
               </div>
--- src/components/window/top/top_window.js
+++ src/components/window/top/top_window.js
@@ -7,8 +7,8 @@ const TopWindow = () => {
     <>
       <div className={styles.ly_center}>
         <div className={styles.bl_topWindow} >
-          <h2 className={`${styles.bl_topWindow_title} ${styles.center}`}>⚖️👑🍹<br/>さんばか1.5周年 <br/>おめでとうございます<br/>🎉🎉🎉</h2>
-          <p className={`${styles.bl_topWindow_content} ${styles.center}`}>本サイトはさんばかが結成されてから<br />1.5周年をお祝いする非公式ファンサイトです.</p>
+          <h2 className={styles.bl_topWindow_title}>⚖️👑🍹<br/>さんばか1.5周年 <br/>おめでとうございます<br/>🎉🎉🎉</h2>
+          <p className={styles.bl_topWindow_content}>本サイトはさんばかが結成されてから<br />1.5周年をお祝いする非公式ファンサイトです.</p>
         </div>
       </div>
       <div className={styles.bl_scroll}>
