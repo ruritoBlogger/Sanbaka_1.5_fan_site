cbda05569b70a5de04f7d61dbeede39c437a08da
rurito0125 <rurito.blog@gmail.com>
2020-10-23 18:47:22 +0900

    top windowをリファクタリング

--- src/components/window/top/top_window.js
+++ src/components/window/top/top_window.js
@@ -5,14 +5,14 @@ const TopWindow = () => {
 
   return (
     <>
-      <div className={styles.root}>
-        <div className={styles.main} >
-          <h2 className={`${styles.title} ${styles.center}`}>⚖️👑🍹<br/>さんばか1.5周年 <br/>おめでとうございます<br/>🎉🎉🎉</h2>
-          <p className={`${styles.content} ${styles.center}`}>本サイトはさんばかが結成されてから<br />1.5周年をお祝いする非公式ファンサイトです.</p>
+      <div className={styles.ly_center}>
+        <div className={styles.bl_topWindow} >
+          <h2 className={`${styles.bl_topWindow_title} ${styles.center}`}>⚖️👑🍹<br/>さんばか1.5周年 <br/>おめでとうございます<br/>🎉🎉🎉</h2>
+          <p className={`${styles.bl_topWindow_content} ${styles.center}`}>本サイトはさんばかが結成されてから<br />1.5周年をお祝いする非公式ファンサイトです.</p>
         </div>
       </div>
-      <div className={`${styles.center} ${styles.only_pc}`}>
-        <a className={`${styles.scroll} ${styles.only_pc}`} href="#">Scroll</a>
+      <div className={styles.bl_scroll}>
+        <a className={styles.scroll} href="#">Scroll</a>
       </div>
     </>
   );
