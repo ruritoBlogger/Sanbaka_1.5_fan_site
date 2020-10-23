83413e1a7b83056ea4349386615d76a8ab217394
rurito0125 <rurito.blog@gmail.com>
2020-10-24 05:46:45 +0900

    hisotry windowの一部を修正

--- src/components/window/history/history_window.js
+++ src/components/window/history/history_window.js
@@ -12,29 +12,29 @@ const HistoryWindow = (props) => {
   }
   return (
     <>
-      <div className={`${styles.root} ${styles.flex_column}`}>
-        <div className={`${styles.main} ${styles.dynamic_flex_reverse}`} >
+      <div className={`${styles.ly_center} ${styles.flex_column}`}>
+        <div className={`${styles.bl_historyWindow} ${styles.flex_row_reverse} ${styles.flex_only_smartphone_column_reverse}`} >
           <div className={styles.flex_column}>
             <div className={styles.flex_row}>
-              <div className={styles.flex_img}>
-                <img className={styles.img_small} src="https://i.ytimg.com/vi/pOXjuyKjD98/hqdefault.jpg" />
+              <div className={styles.bl_window_flexFrame}>
+                <img className={styles.bl_historyWindow_smallImg} src="https://i.ytimg.com/vi/pOXjuyKjD98/hqdefault.jpg" />
               </div>
-              <div className={styles.flex_img}>
-                <img className={styles.img_large} src="https://i.ytimg.com/vi/PcXH2IAXI_w/hqdefault.jpg" />
+              <div className={styles.bl_window_flexFrame}>
+                <img className={styles.bl_historyWindow_largeImg} src="https://i.ytimg.com/vi/PcXH2IAXI_w/hqdefault.jpg" />
               </div>
-              <div className={styles.flex_img}>
-                <img className={styles.img_small} src="https://i.ytimg.com/vi/66Sj7l3sLcM/hqdefault.jpg" />
+              <div className={styles.bl_window_flexFrame}>
+                <img className={styles.bl_historyWindow_smallImg} src="https://i.ytimg.com/vi/66Sj7l3sLcM/hqdefault.jpg" />
               </div>
             </div>
             <div className={styles.flex_row}>
-              <div className={styles.flex_img}>
-                <img className={styles.img_large} src="https://i.ytimg.com/vi/lPW5D3uHFvI/hqdefault.jpg" />
+              <div className={styles.bl_window_flexFrame}>
+                <img className={styles.bl_historyWindow_largeImg} src="https://i.ytimg.com/vi/lPW5D3uHFvI/hqdefault.jpg" />
               </div>
-              <div className={styles.flex_img}>
-                <img className={styles.img_small} src="https://i.ytimg.com/vi/HWcemG6YO_U/hqdefault.jpg" />
+              <div className={styles.bl_window_flexFrame}>
+                <img className={styles.bl_historyWindow_smallImg} src="https://i.ytimg.com/vi/HWcemG6YO_U/hqdefault.jpg" />
               </div>
-              <div className={styles.flex_img}>
-                <img className={styles.img_large} src="https://i.ytimg.com/vi/S7f5htYwawA/hqdefault.jpg" />
+              <div className={styles.bl_window_flexFrame}>
+                <img className={styles.bl_historyWindow_largeImg} src="https://i.ytimg.com/vi/S7f5htYwawA/hqdefault.jpg" />
               </div>
             </div>
           </div>
