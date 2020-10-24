d984a69f0169903a602ac56755374bc0918fd126
rurito0125 <rurito.blog@gmail.com>
2020-10-24 15:31:33 +0900

    member windowを改良

--- src/components/ecosystem/member/member_window.js
+++ src/components/ecosystem/member/member_window.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import AnimationButton from '../../atoms/animationButton';
@@ -8,6 +8,8 @@ import styles from '../window_layout.module.scss';
 
 const MemberWindow = (props) => {
   const history = useHistory();
+  const [ padding, setPadding ] = useState(props.isRight ? styles.paddingLeft : styles.paddingRight);
+  const [ bl_window, setBl_window ] = useState(props.isRight ? `${styles.bl_window} ${styles.bl_memberWindow}` : `${styles.bl_window} ${styles.bl_memberWindow} ${styles.flex_row_reverse}`)
 
   const MoveMemberPage = () => {
     window.scrollTo({top: 0});
@@ -16,48 +18,30 @@ const MemberWindow = (props) => {
 
   return (
     <>
+      <div className={styles.ly_center} >
+        <div className={bl_window} >
+          <MainImage path={'/image/sanbaka.png'} />
+          <div className={padding}>
+            <div>
+              <h2 className={styles.bl_window_title}>さんばかとは</h2>
+              <p className={styles.bl_window_content}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
+              <p className={`${styles.bl_window_content} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
+              <p className={`${styles.bl_window_content} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
+              <p className={`${styles.bl_window_content} ${styles.font_inui}`}>戌亥とこ</p>
+            </div>
+            <div className={`${styles.centerComponent} ${styles.marginTop}`}>
+              <AnimationButton handleClick={ () => MoveMemberPage()} msg="もっと詳しく" />
+            </div>
+          </div>
+        </div>
         {(() => {
-          if( props.isRight ) {
+          if( !props.isRight ) {
             return (
-              <div className={styles.ly_center} >
-                <div className={`${styles.bl_window} ${styles.bl_memberWindow}`} >
-                  <MainImage path={'/image/sanbaka.png'} />
-                  <div className={styles.paddingLeft}>
-                    <div>
-                      <h2 className={styles.bl_window_title}>さんばかとは</h2>
-                      <p className={styles.bl_window_content}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
-                      <p className={`${styles.bl_window_content} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
-                      <p className={`${styles.bl_window_content} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
-                      <p className={`${styles.bl_window_content} ${styles.font_inui}`}>戌亥とこ</p>
-                    </div>
-                    <div className={`${styles.centerComponent} ${styles.marginTop}`}>
-                      <AnimationButton handleClick={ () => MoveMemberPage()} msg="もっと詳しく" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )
-          } else {
-            return (
-              <div className={styles.ly_center} >
-                <div className={`${styles.bl_window} ${styles.bl_memberWindow}`} >
-                  <div className={styles.paddingRight}>
-                    <h2 className={styles.bl_window_title}>さんばかとは</h2>
-                    <p className={styles.bl_window_content}>サンバカーニバルの略であり,<br/> 以下の３人によって結成されたユニット.</p>
-                    <p className={`${styles.bl_window_content} ${styles.font_ange}`}><br/>アンジュ・カトリーナ</p>
-                    <p className={`${styles.bl_window_content} ${styles.font_lize}`}>リゼ・ヘルエスタ</p>
-                    <p className={`${styles.bl_window_content} ${styles.font_inui}`}>戌亥とこ</p>
-                    <p className={styles.bl_window_content}><br/>３人のバカという意味ではないとのこと.</p>
-                  </div>
-                  <MainImage path={'/image/sanbaka.png'} />
-                </div>
-                <div className={styles.ly_center}>
-                  <ScrollNavigator msg="Scroll" />
-                </div>
-              </div>
+              <ScrollNavigator msg="Scroll" />
             )
           }
         })()}
+      </div>
     </>
   );
 };
