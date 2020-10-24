e140f606d7043aff9b67a2edf90d4e756b33a812
rurito0125 <rurito.blog@gmail.com>
2020-10-24 14:32:33 +0900

    メインで表示している画像をAtomsに分離

--- src/components/window/member/member_window.js
+++ src/components/window/member/member_window.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../../parts/button';
 import ScrollNavigator from '../../atoms/scrollNavigator';
+import MainImage from '../../atoms/mainImage';
 import styles from '../window_layout.module.scss';
 
 const MemberWindow = (props) => {
@@ -19,9 +20,7 @@ const MemberWindow = (props) => {
             return (
               <div className={styles.ly_center} >
                 <div className={`${styles.bl_window} ${styles.bl_memberWindow}`} >
-                  <div className={styles.bl_memberWindow_frame}>
-                    <img className={styles.bl_memberWindow_img} src={`${window.location.origin}/image/sanbaka.png`} />
-                  </div>
+                  <MainImage path={'/image/sanbaka.png'} />
                   <div className={styles.paddingLeft}>
                     <div>
                       <h2 className={styles.bl_window_title}>さんばかとは</h2>
@@ -49,11 +48,7 @@ const MemberWindow = (props) => {
                     <p className={`${styles.bl_window_content} ${styles.font_inui}`}>戌亥とこ</p>
                     <p className={styles.bl_window_content}><br/>３人のバカという意味ではないとのこと.</p>
                   </div>
-                  <div>
-                    <div className={styles.bl_memberWindow_frame}>
-                      <img className={styles.bl_memberWindow_img} src={`${window.location.origin}/image/sanbaka.png`} />
-                    </div>
-                  </div>
+                  <MainImage path={'/image/sanbaka.png'} />
                 </div>
                 <div className={styles.ly_center}>
                   <ScrollNavigator msg="Scroll" />
