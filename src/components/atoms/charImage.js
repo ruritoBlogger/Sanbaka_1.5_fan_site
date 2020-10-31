edea2e339fcf39ca1fcae9d071cf2d5878de00c4
rurito0125 <rurito.blog@gmail.com>
2020-10-31 19:58:26 +0900

    refactor: キャラの画像を表示するコンポーネントを分離した

---
+++ src/components/atoms/charImage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './charImage.module.scss';
+
+/**
+ * 各キャラの画像のコンポーネント
+ * 画面の半分ほどの大きさで表示する
+ * @param {string} props.path 表示したい画像のパス
+ * ex: '/image/sanbaka.png'
+ */
+
+const CharImage = (props) => {
+  return (
+    <div className={styles.frame} >
+      <img className={styles.image} src={`${window.location.origin}` + props.path} />
+    </div>
+  )
+}
+
+export default CharImage;
