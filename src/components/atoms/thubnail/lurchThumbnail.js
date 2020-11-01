18821a5601e4de9924872d6f02040c82d4d83e2f
rurito0125 <rurito.blog@gmail.com>
2020-11-01 17:58:32 +0900

    fix: 傾けてサムネイルを表示する部分がスマホ版にてUIが崩れるバグを修正

--- src/components/atoms/thubnail/lurchThumbnail.js
+++ src/components/atoms/thubnail/lurchThumbnail.js
@@ -3,6 +3,7 @@ import styles from './lurchThumbnail.module.scss';
 
 /**
  * youtubeのサムネイルを傾けて表示するコンポーネント
+ * スマホ版で表示する際は傾けない
  * @param {string} props.youtubeID 表示したい画像のyoutubeID
  * @param {bool} props.isSlantRight 右に傾けるかどうか
  */
