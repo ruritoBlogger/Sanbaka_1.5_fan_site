745ec566d3f49a7b79aff77689b9ce41aa968127
ruritoBlogger <rurito.blog@gmail.com>
2020-12-30 18:30:54 +0900

    fix: 画面表示がズレるバグを修正

--- src/components/ecosystem/member/angeWindow.js
+++ src/components/ecosystem/member/angeWindow.js
@@ -17,7 +17,7 @@ const AngeWindow = () => {
       <div className={`${styles.ly_center} ${styles.color_ange}`}>
         <div className={`${styles.bl_window_small} ${styles.flex_column_onlySmartphone}`}>
           <CharMessage msg="おまたせ、待った？" path="/image/ange.png" />
-          <div className={`${styles.paddingLeft_pcOnly} ${styles.marginTop}`}>
+          <div className={styles.paddingLeft_pcOnly}>
             <h3 className={`${styles.bl_window_subtitle}`}>公式美少女錬金術師ライバー</h3>
             <h2 className={`${styles.bl_window_title}`}>アンジュ・カトリーナ</h2>
             <p className={`${styles.bl_window_content}`}>
@@ -29,7 +29,7 @@ const AngeWindow = () => {
               <br />
               実はその研究をしているとかしていないとか.
             </p>
-            <div className={`${styles.flex_row} ${styles.centerComponent} ${styles.marginTop}`}>
+            <div className={`${styles.flex_row} ${styles.centerComponent}`}>
               <IconButton handleClick={() => MoveToYoutube()} link={`${window.location.origin}/image/youtube.png`} />
               <IconButton handleClick={() => MoveToTwitter()} link={`${window.location.origin}/image/twitter.png`} />
             </div>
