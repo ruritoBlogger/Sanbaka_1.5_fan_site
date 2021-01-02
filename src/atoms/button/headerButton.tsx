2ae9c0bf7b1a4a9fd432c663f84531bca41e2537
ruritoBlogger <rurito.blog@gmail.com>
2021-01-02 09:04:40 +0900

    refactor: headerButtonをtsに移行

---
+++ src/atoms/button/headerButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './headerButton.module.scss';
+
+interface Props {
+  /** ボタンの文字 */
+  msg: string;
+  /** ボタンがクリックされた時に呼び出す関数 */
+  handleClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+const HeaderButton: React.VFC<Props> = ({msg, handleClick}) => (
+  <button type="button" className={styles.button} onClick={handleClick}>
+    {msg}
+  </button>
+);
+
+export default HeaderButton;
