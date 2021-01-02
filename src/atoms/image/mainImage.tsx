c365bf3cff38c0a8f05b4e2f20633ec1f3dfe573
ruritoBlogger <rurito.blog@gmail.com>
2021-01-02 09:14:38 +0900

    mainImageをtsに移行

--- src/atoms/image/mainImage.tsx
+++ src/atoms/image/mainImage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from './mainImage.module.scss';
 
+interface Props {
+  // 表示したい画像のパス
+  path: string;
+}
+
 /**
  * 画面に大きく表示する画像のコンポーネント
  * 複数枚画像が重なっている用な表現をしている
- * @param {string} props.path 表示したい画像のパス
- * ex: '/image/sanbaka.png'
  */
 
-const MainImage = (props) => (
+const MainImage: React.VFC<Props> = path => (
   <div className={styles.wrapper}>
     <div className={styles.frame}>
-      <img className={styles.image} src={`${window.location.origin}${props.path}`} />
+      <img className={styles.image} src={`${window.location.origin}${path}`} />
     </div>
   </div>
 );
