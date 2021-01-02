0115d25d8a6052cbc05caea09e42cda52655d10f
ruritoBlogger <rurito.blog@gmail.com>
2021-01-02 22:54:01 +0900

    refactor: rootをtsに移行

--- src/root.tsx
+++ src/root.tsx
@@ -6,18 +6,13 @@ import Member from './pages/member';
 import Road from './pages/road';
 import Site from './pages/site';
 
-const Notfound = () => (
-  <div> 404 not found</div>
-);
-
-const Root = () => (
+const Root: React.VFC = () => (
   <Router>
     <Switch>
-      <Route exact path="/" render={() => <Home />} />
       <Route exact path="/member" render={() => <Member />} />
       <Route exact path="/road" render={() => <Road />} />
       <Route exact path="/site" render={() => <Site />} />
-      <Route component={Notfound} />
+      <Route path="/" render={() => <Home />} />
     </Switch>
   </Router>
 );
