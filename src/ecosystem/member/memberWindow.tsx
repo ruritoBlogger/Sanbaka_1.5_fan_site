d42dcb784ac7e665cf5b54526ae3aa5aa2ab2201
ruritoBlogger <rurito.blog@gmail.com>
2021-01-02 09:57:32 +0900

    refactor: memberWindowをtsに移行

--- src/ecosystem/member/memberWindow.tsx
+++ src/ecosystem/member/memberWindow.tsx
@@ -6,10 +6,15 @@ import ScrollNavigator from '../../atoms/scrollNavigator';
 import MainImage from '../../atoms/image/mainImage';
 import styles from '../window_layout.module.scss';
 
-const MemberWindow = (props) => {
+interface Props {
+  // 画像を右側に表示するかどうか
+  isRight: Boolean;
+}
+
+const MemberWindow: React.VFC<Props> = isRight => {
   const history = useHistory();
-  const [padding, setPadding] = useState(props.isRight ? `${styles.bl_window_smallBlock} ${styles.paddingLeft_pcOnly}` : `${styles.bl_window_smallBlock} ${styles.paddingRight_pcOnly}`);
-  const [bl_window, setBl_window] = useState(props.isRight ? `${styles.bl_window} ${styles.bl_memberWindow}` : `${styles.bl_window} ${styles.bl_memberWindow} ${styles.flex_row_reverse}`);
+  const [padding, setPadding] = useState(isRight ? `${styles.bl_window_smallBlock} ${styles.paddingLeft_pcOnly}` : `${styles.bl_window_smallBlock} ${styles.paddingRight_pcOnly}`);
+  const [bl_window, setBl_window] = useState(isRight ? `${styles.bl_window} ${styles.bl_memberWindow}` : `${styles.bl_window} ${styles.bl_memberWindow} ${styles.flex_row_reverse}`);
 
   const MoveMemberPage = () => {
     window.scrollTo({ top: 0 });
@@ -41,7 +46,7 @@ const MemberWindow = (props) => {
               <p className={`${styles.bl_memberWindow_content} ${styles.font_inui}`}>戌亥とこ</p>
             </div>
             {(() => {
-              if (props.isRight) {
+              if (isRight) {
                 return (
                   <div className={`${styles.centerComponent} ${styles.marginTop}`}>
                     <AnimationButton handleClick={() => MoveMemberPage()} msg="もっと詳しく" />
@@ -53,7 +58,7 @@ const MemberWindow = (props) => {
         </div>
         {/* HACK: 呼び出し元の引数名を改良する */}
         {(() => {
-          if (!props.isRight) {
+          if (!isRight) {
             return (
               <ScrollNavigator msg="Scroll" isOnlyPC={false} />
             );
