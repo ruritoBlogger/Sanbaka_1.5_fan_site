40534e9f33a8a31defb6938b1fd4bff376ff37c0
ruritoBlogger <rurito.blog@gmail.com>
2021-01-02 09:07:57 +0900

    refactor: moveButtonをtsに移行

---
+++ src/atoms/button/moveButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './moveButton.module.scss';
+
+interface Props {
+  /** ボタンがクリックされた時に呼び出す関数 */
+  handleClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+/*
+ * スクロールする画面の一番上に戻るボタン
+ */
+const MoveButton: React.VFC<Props> = ({handleClick}) => (
+  <button type="button" className={styles.button} onClick={handleClick}>
+    top
+  </button>
+);
+
+export default MoveButton;
