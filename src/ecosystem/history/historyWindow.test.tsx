512a1e892c99ec18b9a94b352dc517b76c474989
0918nobita <nobita.0918@gmail.com>
2021-01-05 14:18:37 +0900

    test: HistoryWindow のテストを TypeScript に移行

--- src/ecosystem/history/historyWindow.test.tsx
+++ src/ecosystem/history/historyWindow.test.tsx
@@ -5,7 +5,7 @@ import HistoryWindow from './historyWindow';
 
 describe('HistoryWindow Component', () => {
   test('check HistoryWindow behavior', () => {
-    render(<HistoryWindow isTop={true} />);
+    render(<HistoryWindow isTop />);
     expect(screen.getByText('1.5周年', { exact: false })).toBeInTheDocument;
   });
 });
