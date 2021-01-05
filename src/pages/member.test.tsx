e1f52da37d13bb5e81ed974df137f25fc29a6ea0
0918nobita <nobita.0918@gmail.com>
2021-01-05 14:31:03 +0900

    test: Member page のテストを TypeScript に移行し、内容を修正

--- src/pages/member.test.tsx
+++ src/pages/member.test.tsx
@@ -6,6 +6,6 @@ import Member from './member';
 describe('Member', () => {
   test('check Member behavior', () => {
     render(<Member />);
-    expect(screen.getByText('Member', { exact: false })).toBeInTheDocument;
+    expect(screen.getByText('ユニット', { exact: false })).toBeInTheDocument;
   });
 });
