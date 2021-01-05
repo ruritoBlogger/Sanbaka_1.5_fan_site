58ebd2b002d8a244044ef9abf0e9468c516e6db7
ruritoBlogger <rurito.blog@gmail.com>
2021-01-05 17:58:11 +0900

    chore: ts用のeslintの設定を更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
   extends: 'airbnb',
   ignorePatterns: ['public'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json']
+  },
   plugins: [
-    'react',
-    'jsx-a11y',
-    'import',
+    '@typescript-eslint',
   ],
   rules: {
     'no-undef': 'off',
     'no-unused-expressions': 'off',
     'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
   },
-  overrides: [
-    {
-      files: ['development.js'],
-      rules: {
-        'import/no-extraneous-dependencies': 'off',
-      },
-    },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
 };
