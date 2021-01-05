6a8ed0829bd60847ed4eb60e90ee970d15c99096
0918nobita <nobita.0918@gmail.com>
2021-01-05 14:56:50 +0900

    refactor: CharAnimationButton の Props の型をより厳密にした

--- src/atoms/button/charAnimationButton.tsx
+++ src/atoms/button/charAnimationButton.tsx
@@ -6,8 +6,8 @@ interface Props {
   msg: string;
   // ボタンがクリックされた時に呼び出す関数
   handleClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  // どのテーマ色を用いるのか(['ange', 'lize', 'inui'])
-  theme: string;
+  // どのテーマ色を用いるのか
+  theme: 'ange' | 'lize' | 'inui';
 }
 
 /**
@@ -20,13 +20,13 @@ const CharAnimationButton: React.VFC<Props> = ({theme, handleClick, msg}) => {
    * コンポーネントが描画されるタイミングでテーマ色を決定する
    */
   useEffect(() => {
-    if (theme == 'ange') {
-      setStyle(`${styles.color_ange} ${styles.button}`);
-    } else if (theme == 'lize') {
-      setStyle(`${styles.color_lize} ${styles.button}`);
-    } else {
-      setStyle(`${styles.color_inui} ${styles.button}`);
-    }
+    const themeClassName = ({
+      ange: styles.color_ange,
+      lize: styles.color_lize,
+      inui: styles.color_inui,
+    })[theme];
+
+    setStyle(`${themeClassName} ${styles.button}`);
   }, []);
 
   return (
