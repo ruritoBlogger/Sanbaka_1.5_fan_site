2fe6e167907420c21b9a3497b4fd707043f67a1f
ruritoBlogger <rurito.blog@gmail.com>
2021-01-06 15:18:15 +0900

    fix: 余計なセミコロンを削除

--- babel.config.ts
+++ babel.config.ts
@@ -3,7 +3,7 @@ import { TransformOptions } from '@babel/core';
 export const devBabelConfig: TransformOptions = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { targets: { node: 'current' }, runtime: 'automatic' },],
+    ['@babel/preset-react', { targets: { node: 'current' }, runtime: 'automatic' }],
     '@babel/preset-typescript',
   ],
 };
