f0b116063d4aea4f4b0e805f629f90d1b709b511
ruritoBlogger <rurito.blog@gmail.com>
2021-01-06 16:33:50 +0900

    chore: eslintをjsにもtsにも動作するように修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,39 @@
 module.exports = {
   extends: 'airbnb',
   ignorePatterns: ['public', 'node_modules'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json'
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-    'no-undef': 'off',
-    'no-unused-expressions': 'off',
-    '@typescript-eslint/unbound-method': 'off',
-    'react/destructuring-assignment': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 'off',
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react/recommended',
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json'
+      },
+      plugins: [
+        '@typescript-eslint',
+      ],
+      rules: {
+        'no-undef': 'off',
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/unbound-method': 'off',
+        'import/extensions': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': 'off',
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:jsx-a11y/recommended',
+        'plugin:react/recommended',
+      ],
+    },
   ],
 };
