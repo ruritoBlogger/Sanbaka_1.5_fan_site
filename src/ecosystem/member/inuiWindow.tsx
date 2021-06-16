26b968584587ab6ad7411075e47c4ac9f05b83b7
ruritoBlogger <rurito.blog@gmail.com>
2021-06-16 14:03:13 +0900

    fix: 不必要なwindowの使用を辞めた

--- src/ecosystem/member/inuiWindow.tsx
+++ src/ecosystem/member/inuiWindow.tsx
@@ -2,62 +2,40 @@ import IconButton from "../../atoms/button/iconButton";
 import CharMessage from "../../molecules/charMessage";
 import styles from "../window_layout.module.scss";
 
-/**
- * 戌亥の紹介ページ
- */
-const InuiWindow: React.FC = () => {
-  /**
-   * 戌亥のyoutubeチャンネルに遷移する
-   */
-  const MoveToYoutube = () => {
-    window.open(
-      "https://www.youtube.com/channel/UCXRlIK3Cw_TJIQC5kSJJQMg?sub_confirmation=1",
-      "_blank"
-    );
-  };
-
-  /**
-   * 戌亥のTwitterページに遷移する
-   */
-  const MoveToTwitter = () => {
-    window.open("https://twitter.com/inui_toko", "_blank");
-  };
-
-  return (
-    <div className={`${styles.ly_center} ${styles.color_inui}`}>
-      <div
-        className={`${styles.bl_window_small} ${styles.flex_column_onlySmartphone}`}
-      >
-        <CharMessage msg="アハーーー↑↑↑" path="/image/inui.png" />
-        <div className={styles.paddingLeft_pcOnly}>
-          <h3 className={`${styles.bl_window_subtitle}`}>
-            公式つよつよケルベロス
-          </h3>
-          <h2 className={`${styles.bl_window_title}`}>戌亥とこ</h2>
-          <p className={`${styles.bl_window_content}`}>
-            寂れた和風喫茶で働く女の子. <br />
-            店長の趣味でメイド服を着せられている.
-            <br />
-            足下に見え隠れする尻尾が本物かどうかは
-            <br />
-            触れた人のみぞ知る.
-            <br />
-            満月の夜は何か不思議な事が起きるらしい？
-          </p>
-          <div className={`${styles.flex_row} ${styles.centerComponent}`}>
-            <IconButton
-              handleClick={() => MoveToYoutube()}
-              link={`${window.location.origin}/image/youtube.png`}
-            />
-            <IconButton
-              handleClick={() => MoveToTwitter()}
-              link={`${window.location.origin}/image/twitter.png`}
-            />
-          </div>
+const InuiWindow: React.FC = () => (
+  <div className={`${styles.ly_center} ${styles.color_inui}`}>
+    <div
+      className={`${styles.bl_window_small} ${styles.flex_column_onlySmartphone}`}
+    >
+      <CharMessage msg="アハーーー↑↑↑" path="/image/inui.png" />
+      <div className={styles.paddingLeft_pcOnly}>
+        <h3 className={`${styles.bl_window_subtitle}`}>
+          公式つよつよケルベロス
+        </h3>
+        <h2 className={`${styles.bl_window_title}`}>戌亥とこ</h2>
+        <p className={`${styles.bl_window_content}`}>
+          寂れた和風喫茶で働く女の子. <br />
+          店長の趣味でメイド服を着せられている.
+          <br />
+          足下に見え隠れする尻尾が本物かどうかは
+          <br />
+          触れた人のみぞ知る.
+          <br />
+          満月の夜は何か不思議な事が起きるらしい？
+        </p>
+        <div className={`${styles.flex_row} ${styles.centerComponent}`}>
+          <IconButton
+            imgLink="/image/youtube.png"
+            url="https://www.youtube.com/channel/UCXRlIK3Cw_TJIQC5kSJJQMg?sub_confirmation=1"
+          />
+          <IconButton
+            imgLink="/image/twitter.png"
+            url="https://twitter.com/inui_toko"
+          />
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default InuiWindow;
--- src/pages/road/index.tsx
+++ src/pages/road/index.tsx
@@ -26,7 +26,7 @@ const Road: React.VFC = () => {
           title="さんばかサーバー第一回"
           text="さんばかの初コラボ."
           day="2019/03/30"
-          youtube_data={[
+          youtubeData={[
             { time: 44, key: "ZlpG8R_biw4" },
             { time: 25, key: "Wh2bMyxrXb0" },
             { time: 1, key: "pOXjuyKjD98" },
@@ -43,7 +43,7 @@ const Road: React.VFC = () => {
           title="さんばかサーバー第二回"
           text="さんばかサーバーでの二回目のコラボ. "
           day="2019/04/09"
-          youtube_data={[
+          youtubeData={[
             { time: 63, key: "y2TbN8WUmb4" },
             { time: 65, key: "PcXH2IAXI_w" },
             { time: 52, key: "MnDPwcFm8Ho" },
@@ -60,7 +60,7 @@ const Road: React.VFC = () => {
           title="にじさんじサーバー第一回"
           text="初めてのにじさんじサーバーでのコラボ."
           day="2019/05/04"
-          youtube_data={[
+          youtubeData={[
             { time: 142, key: "66Sj7l3sLcM" },
             { time: 145, key: "t4nJkrU2-M0" },
             { time: 68, key: "RCEHT3VxkM4" },
@@ -77,7 +77,7 @@ const Road: React.VFC = () => {
           title="にじさんじサーバー第二回"
           text="にじさんじ鯖の観光の続き. "
           day="2019/05/19"
-          youtube_data={[
+          youtubeData={[
             { time: 32, key: "UYvM47QCxZg" },
             { time: 40, key: "jB3JNinXYe0" },
             { time: 37, key: "lPW5D3uHFvI" },
@@ -94,7 +94,7 @@ const Road: React.VFC = () => {
           title="にじさんじサーバー第三回"
           text="風雲さんばか城, 築城開始！"
           day="2019/06/10"
-          youtube_data={[
+          youtubeData={[
             { time: 28, key: "AoPQ3D4NrlI" },
             { time: 30, key: "HWcemG6YO_U" },
             { time: 26, key: "9VkOIfGXrh4" },
@@ -111,7 +111,7 @@ const Road: React.VFC = () => {
           title="にじさんじサーバー第四回"
           text="風雲さんばか城完成に向けて "
           day="2019/08/25"
-          youtube_data={[
+          youtubeData={[
             { time: 92, key: "S7f5htYwawA" },
             { time: 80, key: "-Tsu0cLkEJg" },
             { time: 90, key: "kAxyrXTANYQ" },
@@ -128,7 +128,7 @@ const Road: React.VFC = () => {
           title="にじ鯖夏祭り"
           text="にじ鯖夏祭りにさんばかが参加."
           day="2020/08/22"
-          youtube_data={[
+          youtubeData={[
             { time: 4, key: "9r_1yLnqS-0" },
             { time: 3, key: "BWcEKleNUV0" },
             { time: 55, key: "QM6QCi_fB0k" },
