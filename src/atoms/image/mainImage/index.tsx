e7bd3452e36ec81276394e944878bf85d8442688
ruritoBlogger <rurito.blog@gmail.com>
2021-06-16 17:33:32 +0900

    refactor: mainImageを改修した

--- src/atoms/image/mainImage/index.tsx
+++ src/atoms/image/mainImage/index.tsx
@@ -1,16 +1,16 @@
-import styles from "./mainImage.module.scss";
+import styles from "./index.module.scss";
 
 interface Props {
-  // 表示したい画像のパス
   path: string;
 }
 
 /**
  * 画面に大きく表示する画像のコンポーネント
  * 複数枚画像が重なっている用な表現をしている
+ * @param {string} path: 表示したい画像のパス
  */
 
-const MainImage: React.VFC<Props> = ({ path }) => (
+const MainImage: React.FC<Props> = ({ path }) => (
   <div className={styles.wrapper}>
     <div className={styles.frame}>
       <img className={styles.image} src={path} alt="集合写真" />
