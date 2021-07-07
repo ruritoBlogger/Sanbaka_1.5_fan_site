c66113a536b41804984cdc7d23dc0e4c1359083c
ruritoBlogger <rurito.blog@gmail.com>
2021-07-08 08:39:40 +0900

    refactor: balloonを改修

--- src/atoms/balloon/index.tsx
+++ src/atoms/balloon/index.tsx
@@ -1,14 +1,14 @@
-import styles from './balloon.module.scss';
+import styles from './index.module.scss';
 
 interface Props {
-  // 吹き出しに挿入する文字
   msg: string;
 }
 
 /**
  * 吹き出しを表示するコンポーネント
+ * @param {string} msg: 吹き出しに挿入する文字
  */
-const Balloon: React.VFC<Props> = ({ msg }) => (
+const Balloon: React.FC<Props> = ({ msg }) => (
   <div className={styles.balloon}>
     <p className={styles.char_msg}>{msg}</p>
   </div>
