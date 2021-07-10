6163515c05add3e5f20970cc85a619e428e84725
ruritoBlogger <rurito.blog@gmail.com>
2021-07-11 06:53:22 +0900

    fix: 読み込み時に全ての内容を表示するように変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { isMobile } from "react-device-detect";
 import TopWindow from "../ecosystem/top/topWindow";
 import MemberWindow from "../ecosystem/member/memberWindow";
-import HistoryWindow from "../ecosystem/history/historyWindow";
+import HistoryWindow from "../ecosystem/history/";
 import Header from "../organisms/header";
 
-gsap.registerPlugin(ScrollTrigger);
-
 const Home: React.FC = () => {
-  const headerRef = useRef<HTMLDivElement>(null);
-  const revealRefs = useRef<HTMLDivElement[]>([]);
-  let scrollSize = 20;
-
   const windows = [
     {
       key: "top",
@@ -29,51 +19,11 @@ const Home: React.FC = () => {
     },
   ];
 
-  useEffect(() => {
-    if (isMobile) {
-      scrollSize = 100;
-    }
-
-    gsap.from(headerRef.current, {
-      autoAlpha: 0,
-      ease: "none",
-      delay: 1,
-    });
-
-    revealRefs.current.forEach((el, index) => {
-      gsap.fromTo(
-        el,
-        {
-          autoAlpha: 0,
-        },
-        {
-          duration: 1,
-          autoAlpha: 1,
-          ease: "none",
-          scrollTrigger: {
-            id: `section-${index + 1}`,
-            trigger: el,
-            start: `top center+=${scrollSize}`,
-            toggleActions: "play none none reverse",
-          },
-        }
-      );
-    });
-  }, []);
-
-  const addToRefs = (el: HTMLDivElement) => {
-    if (el && !revealRefs.current.includes(el)) {
-      revealRefs.current.push(el);
-    }
-  };
-
   return (
     <>
       <Header />
       {windows.map(({ key, content }) => (
-        <div key={key} ref={addToRefs}>
-          {content}
-        </div>
+        <div key={key}>{content}</div>
       ))}
     </>
   );
